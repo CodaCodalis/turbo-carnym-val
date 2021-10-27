83483a66cb2bb9f8a4e1e84be318507d46d483cd
CodaCodalis <larah.wuxus@gmail.com>
2021-10-27 14:50:57 +0200

    quiz.sql korrigiert

--- db/quiz.sql
+++ db/quiz.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.0.4deb2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Erstellungszeit: 27. Okt 2021 um 12:40
--- Server-Version: 8.0.26-0ubuntu0.20.04.2
--- PHP-Version: 7.4.3
+-- Erstellungszeit: 27. Okt 2021 um 14:49
+-- Server-Version: 10.5.11-MariaDB-1
+-- PHP-Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -29,11 +28,11 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `antworten` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `antworttext` varchar(255) NOT NULL,
   `wahrheit` tinyint(1) NOT NULL,
-  `frage_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `frage_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -42,10 +41,10 @@ CREATE TABLE `antworten` (
 --
 
 CREATE TABLE `fragen` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `fragetext` text NOT NULL,
-  `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -54,9 +53,9 @@ CREATE TABLE `fragen` (
 --
 
 CREATE TABLE `frage_kategorie` (
-  `frage_id` int NOT NULL,
-  `kategorie_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `frage_id` int(11) NOT NULL,
+  `kategorie_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -65,9 +64,9 @@ CREATE TABLE `frage_kategorie` (
 --
 
 CREATE TABLE `kategorien` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -76,9 +75,9 @@ CREATE TABLE `kategorien` (
 --
 
 CREATE TABLE `rollen` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `rollen`
@@ -96,10 +95,10 @@ INSERT INTO `rollen` (`id`, `name`) VALUES
 --
 
 CREATE TABLE `user` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(32) NOT NULL,
-  `passwort` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `passwort` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Daten für Tabelle `user`
@@ -118,9 +117,9 @@ INSERT INTO `user` (`id`, `name`, `passwort`) VALUES
 --
 
 CREATE TABLE `user_rolle` (
-  `user_id` int NOT NULL,
-  `rolle_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  `user_id` int(11) NOT NULL,
+  `rolle_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indizes der exportierten Tabellen
@@ -180,31 +179,31 @@ ALTER TABLE `user_rolle`
 -- AUTO_INCREMENT für Tabelle `antworten`
 --
 ALTER TABLE `antworten`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT für Tabelle `fragen`
 --
 ALTER TABLE `fragen`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT für Tabelle `kategorien`
 --
 ALTER TABLE `kategorien`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT für Tabelle `rollen`
 --
 ALTER TABLE `rollen`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints der exportierten Tabellen
@@ -214,27 +213,27 @@ ALTER TABLE `user`
 -- Constraints der Tabelle `antworten`
 --
 ALTER TABLE `antworten`
-  ADD CONSTRAINT `antworten_ibfk_1` FOREIGN KEY (`frage_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `antworten_ibfk_1` FOREIGN KEY (`frage_id`) REFERENCES `fragen` (`id`);
 
 --
 -- Constraints der Tabelle `fragen`
 --
 ALTER TABLE `fragen`
-  ADD CONSTRAINT `fragen_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `fragen_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 --
 -- Constraints der Tabelle `frage_kategorie`
 --
 ALTER TABLE `frage_kategorie`
-  ADD CONSTRAINT `frage_kategorie_ibfk_1` FOREIGN KEY (`frage_id`) REFERENCES `fragen` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `frage_kategorie_ibfk_2` FOREIGN KEY (`kategorie_id`) REFERENCES `kategorien` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `frage_kategorie_ibfk_1` FOREIGN KEY (`frage_id`) REFERENCES `fragen` (`id`),
+  ADD CONSTRAINT `frage_kategorie_ibfk_2` FOREIGN KEY (`kategorie_id`) REFERENCES `kategorien` (`id`);
 
 --
 -- Constraints der Tabelle `user_rolle`
 --
 ALTER TABLE `user_rolle`
-  ADD CONSTRAINT `user_rolle_ibfk_1` FOREIGN KEY (`rolle_id`) REFERENCES `rollen` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  ADD CONSTRAINT `user_rolle_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+  ADD CONSTRAINT `user_rolle_ibfk_1` FOREIGN KEY (`rolle_id`) REFERENCES `rollen` (`id`),
+  ADD CONSTRAINT `user_rolle_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
