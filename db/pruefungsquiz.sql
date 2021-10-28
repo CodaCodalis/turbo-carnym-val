6333731bb88f8001f5ba897ab88be67fd12e1d7d
Cheesecake208 <filumbi@gmx.de>
2021-10-28 09:09:02 +0200

    pruefungsquiz sql korrigiert

--- db/pruefungsquiz.sql
+++ db/pruefungsquiz.sql
@@ -1,11 +1,9 @@
 CREATE DATABASE `pruefungsquiz`;
 
-CREATE TABLE `antworten` (
-	`id` int PRIMARY KEY AUTO_INCREMENT NOT NULL,
-	`antworttext` varchar(255) NOT NULL,
-	`wahrheit` tinyint(1) NOT NULL,
-	`frage_id` int NOT NULL,
-	FOREIGN KEY (`frage_id`) REFERENCES `fragen` (`id`)
+CREATE TABLE `user` (
+ 	`id` int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+ 	`name` varchar(32) NOT NULL,
+  	`passwort` varchar(32) NOT NULL
 );
 
 CREATE TABLE `fragen` (
@@ -15,11 +13,12 @@ CREATE TABLE `fragen` (
 	FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
 
-CREATE TABLE `frage_kategorie` (
-  	`frage_id` int NOT NULL,
-  	`kategorie_id` int NOT NULL,
-	FOREIGN KEY (`frage_id`) REFERENCES `fragen` (`id`),
-	FOREIGN KEY (`kategorie_id`) REFERENCES `kategorien` (`id`)
+CREATE TABLE `antworten` (
+	`id` int PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	`antworttext` varchar(255) NOT NULL,
+	`wahrheit` tinyint(1) NOT NULL,
+	`frage_id` int NOT NULL,
+	FOREIGN KEY (`frage_id`) REFERENCES `fragen` (`id`)
 );
 
 CREATE TABLE `kategorien` (
@@ -27,20 +26,17 @@ CREATE TABLE `kategorien` (
   	`name` varchar(32) NOT NULL
 );
 
-
 CREATE TABLE `rollen` (
   	`id` int PRIMARY KEY AUTO_INCREMENT NOT NULL,
   	`name` varchar(32) NOT NULL
-)
+);
 
-INSERT INTO `rollen` (`id`, `name`) VALUES
-	(1, 'user'),
-	(2, 'admin');
 
-CREATE TABLE `user` (
- 	`id` int PRIMARY KEY AUTO_INCREMENT NOT NULL,
- 	`name` varchar(32) NOT NULL,
-  	`passwort` varchar(32) NOT NULL
+CREATE TABLE `frage_kategorie` (
+  	`frage_id` int NOT NULL,
+  	`kategorie_id` int NOT NULL,
+	FOREIGN KEY (`frage_id`) REFERENCES `fragen` (`id`),
+	FOREIGN KEY (`kategorie_id`) REFERENCES `kategorien` (`id`)
 );
 
 CREATE TABLE `user_rolle` (
@@ -49,6 +45,10 @@ CREATE TABLE `user_rolle` (
 	FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
 	FOREIGN KEY (`rolle_id`) REFERENCES `rollen` (`id`)
 );
+
+INSERT INTO `rollen` (`id`, `name`) VALUES
+	(1, 'user'),
+	(2, 'admin');
 
 INSERT INTO `user` (`id`, `name`, `passwort`) VALUES
 	(1, 'Blub', 'Test'),
