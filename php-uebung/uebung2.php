cadf5052b4fd6ce7a14d06016ccfe61083187c5a
CodaCodalis <larah.wuxus@gmail.com>
2021-11-01 09:20:00 +0100

    uebung 2

---
+++ php-uebung/uebung2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="de">
+	<head>
+      <link href="css/style.css" type="text/css" rel="stylesheet">
+  	</head>
+	
+  	<body>
+      	<h1>Hallo Welt!</h1>
+      <div id="uebung1">
+    	<?php
+        	# Das Doppelkreuz ist neben dem Doppelslash eine Möglichkeit, Codezeilen in php-Blöcken auszukommentieren. 
+        	# Mehrzeilige Kommentare können mit Slash-Stern eingeleitet und Stern-Slash wieder ausgeleitet werden.
+			// Uebung 1:
+        	/*
+            Mit echo können Strings in php-Code-Blöcken ausgegeben werden.
+            */
+				echo "Hallo Welt!";
+
+		?>
+      </div>
+  	</body>
+</html>
