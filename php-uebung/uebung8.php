dfe914377c5f828afac1e40c234f0f7c03c6e14c
yanko13 <92911888+yanko13@users.noreply.github.com>
2021-11-02 09:59:02 +0100

    uebung8 -Validierung
    uebung8 -Validierung

--- php-uebung/uebung8.php
+++ php-uebung/uebung8.php
@@ -8,23 +8,29 @@
   	</head>
 	
   	<body>
-      	<h1>Übung 7</h1>
+      	<h1>Übung 8</h1>
       <div id="uebung">
-    	<?php
-            echo "Haus:<br>";
-            $schuppen = new Haus("Schuppen", "rosa", 2, 100, true);
 
-            $schuppen->status();
-            
-            $schuppen->anstreichen("blau");
-            $schuppen->aufstocken(1);
-            $schuppen->anbauen(20);
-            
-            $schuppen->status();
-            
-            $schuppen->abreissen();
-            
-            $schuppen->status();
+        <div id="formular">
+        
+            <label for="Frage1">Frage1</label><input type="text" id="frage1"><br>
+            <label for="name">Name</label><input type="text" name="name" id="name" class="eingabe"
+                placeholder="z.B. Max Mustermann"><br>
+            <label for="wohnort">Wohnort</label><input type="text" name="wohnort" id="wohnort" class="eingabe"
+                placeholder="z.B. London"><br>
+            <label for="email">Email</label><input type="email" name="email" id="email" class="eingabe"
+                placeholder="z.B. max.mustermann@mail.org"><br>
+            <label for="alter">Alter</label><input type="number" name="alter" id="alter" class="eingabe"
+                placeholder="z.B. 83"><br>
+            <label for="formular">Formular</label>
+
+            <input type="button" name="send" id="send" onclick="sndBtnClick();" value="Daten absenden">
+            <input type="reset" name="reset" id="reset"
+                onclick="  document.getElementsByClassName('ausgabe')[0].setAttribute('style', 'display: none');">
+
+        </div>
+    
+    	<?php
 		?>
       </div>
       <a href="index.php">zurück</a>
