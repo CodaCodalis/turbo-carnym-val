2dceb6522ae7592e736221f827de4a40ec738a26
yanko13 <92911888+yanko13@users.noreply.github.com>
2021-11-02 13:50:50 +0100

    uebung_8 update

--- php-uebung/classes/validate.php
+++ php-uebung/classes/validate.php
@@ -10,6 +10,15 @@
             return $valid;
         } 
 
+        /*
+        die Funktion is_int() überprüft ob es sich bei $number um einen Integer handelt und liefert über return true oder false zurück.
+        Der Cast nach int ist notwendig da das Formularfeld nur String zurückliefert.
+        */
+        function validateNumber($number){
+
+            return is_int((int) $number);
+        }
+
     }
 
 ?>
--- php-uebung/uebung8.php
+++ php-uebung/uebung8.php
@@ -14,23 +14,36 @@
         <div id="formular">
             <form action="uebung8.php" method="POST">
                 <label for="Frage1">Frage1</label><input type="text" name="text" id="text"><br>
-                <label for="nummer">Nummer</label><input type="number" name="nummer" id="nummer" class="eingabe"
+                <label for="number">Nummer</label><input type="number" name="number" id="number" class="eingabe"
                 placeholder="nur Zahlen"><br>
                 <input type="submit" name="send" id="send" value="Daten absenden">
             </form>
         </div>
     
     	<?php
-                if(isset($_POST['text'])){
+                if(isset($_POST['text']) AND $_POST['text']){
                     $text = $_POST['text'];
 
                     $validate = new Validate();
                     if(!$validate->validateText($text)){
-                        echo "Falsche Eingabe, nur Buchstaben, Zahlen sowie die Zeichen (?.,-_) sind erlaubt.";
+                        echo "<br>Falsche Eingabe, nur Buchstaben, Zahlen sowie die Zeichen (?.,-_) sind erlaubt.";
                     }else{
-                        echo "Eingabe valide!";
+                        echo "<br>Eingabe der Frage valide!";
                     }
                 }
+
+                // AND $_POST['number'] überprüft ob nichts (Null) eingeben wurde und lässt das statement dann false werden
+                if(isset($_POST['number']) AND $_POST['number']){
+                    $number = $_POST['number'];
+                    $validate = new Validate();
+                    if(!$validate->validateNumber($number)){
+                        echo "<br>Falsche Eingabe, nur Zahlen sind erlaubt.";
+                    }else{
+                        echo "<br>Eingabe der Zahl valide!";
+                    }
+                }
+
+                
                 
 		?>
       </div>
