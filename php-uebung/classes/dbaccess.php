bf2b4f106a980944b226e56c594b38802f16e7db
gitMat123 <92911809+gitMat123@users.noreply.github.com>
2021-11-04 14:34:36 +0100

    Update dbaccess.php

--- php-uebung/classes/dbaccess.php
+++ php-uebung/classes/dbaccess.php
@@ -56,8 +56,9 @@ public function show_content($table){
         $query = "SELECT * from $table";
         $result = $this->mysqli->query($query);
         /* numeric array */
-        $row = $result->fetch_array(MYSQLI_NUM);
+        while($row = $result->fetch_array(MYSQLI_NUM)){
         printf("%s (%s)\n", $row[0], $row[1]);
+        }
         /* associative array */
         $row = $result->fetch_array(MYSQLI_ASSOC);
         printf("%s (%s)\n", $row["name"], $row["name"]); 
