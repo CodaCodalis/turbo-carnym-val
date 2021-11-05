3ef6671db705b16c86bfd9c1e663719ec031118c
CodaCodalis <larah.wuxus@gmail.com>
2021-11-05 08:43:50 +0100

    index.php verschoenert

--- php-uebung/index.php
+++ php-uebung/index.php
@@ -6,17 +6,19 @@
 	
   	<body>
       	<h1>Willkommen</h1>
-      		<a href="uebung1.php">Übung 1</a><br>
-      		<a href="uebung2.php">Übung 2</a><br>
-      		<a href="uebung3.php">Übung 3</a><br>
-      		<a href="uebung4.php">Übung 4</a><br>
-       		<a href="uebung5.php">Übung 5</a><br>
-      		<a href="uebung6.php">Übung 6</a><br>
-      		<a href="uebung7.php">Übung 7</a><br>
-      		<a href="uebung8.php">Übung 8</a><br>
-			<a href="uebung9.php">Übung 9</a><br>
-			<a href="uebung10.php">Übung 10</a><br>
-			<a href="dbAbfragen.php">DB-Abfragen</a><br>
+      	<div id="indexlinks">
+      		<a href="uebung1.php">Übung 1: Textausgabe mit HTML und PHP</a><br>
+      		<a href="uebung2.php">Übung 2: Funktionen</a><br>
+      		<a href="uebung3.php">Übung 3: Schleifen</a><br>
+      		<a href="uebung4.php">Übung 4: Debugging</a><br>
+       		<a href="uebung5.php">Übung 5: Objekt-orientiertes Programmieren</a><br>
+      		<a href="uebung6.php">Übung 6: Klasse und Objekte</a><br>
+      		<a href="uebung7.php">Übung 7: Konstruktoren und Destruktoren</a><br>
+      		<a href="uebung8.php">Übung 8: Validierung</a><br>
+			<a href="uebung9.php">Übung 9: Vererbung</a><br>
+			<a href="uebung10.php">Übung 10: Dateien und Verzeichnisse</a><br>
+			<a href="dbAbfragen.php">Übung 11: Datenbankanbindung</a><br>
+        </div>
     <?php
 	?>
   	</body>
