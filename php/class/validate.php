f743deabf8b94f6423c01e3f2fb8543a348c8cbf
yanko13 <92911888+yanko13@users.noreply.github.com>
2021-12-06 09:07:06 +0100

    Update validate.php

--- php/class/validate.php
+++ php/class/validate.php
@@ -4,7 +4,7 @@
 
         function validateText($text){
             $valid = false;
-            if(is_string($text) AND strlen($text)<300 AND preg_match('~^[-a-z0-9_:%? .,!]+$~i',$text)){
+            if(is_string($text) AND strlen($text)<300 AND preg_match('~^[-a-z0-9äüöß_:%? .,!]+$~i',$text)){
                 $valid = true;
             }
             return $valid;
