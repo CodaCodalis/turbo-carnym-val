0ae9200caee9a0ee6258a80126aafc8c2825a313
yanko13 <92911888+yanko13@users.noreply.github.com>
2021-12-06 10:22:17 +0100

    update quiz css

--- php/kategoriequiz.php
+++ php/kategoriequiz.php
@@ -87,8 +87,8 @@ $DB_CONNECTION = new Database();
                         echo "<div id='fragekarte'>";
                         $frage_id = $_SESSION['selectedCategoryQuestions'][$_SESSION['frageCount']];
                         $kategorie = $DB_CONNECTION->get_cat_from_question($frage_id);
-                        echo "<div id='FrageInfo'><div id='frageYvonX'>Frage ".$x." von ".$_SESSION['anzahlAuswahlFragen']."</div>";
-                        echo "<div id='kategorieAusgabe'>Kategorie: ".$kategorie[0]."</div></div>";
+                        echo "<div id='FrageInfo'><div id='kategorieAusgabe'>Kategorie: ".$kategorie[0]."</div>";
+                        echo "<div id='frageYvonX'>Frage ".$x." von ".$_SESSION['anzahlAuswahlFragen']."</div></div>";
                         
         
                         $DB_CONNECTION->show_questions($_SESSION['selectedCategoryQuestions'],$_SESSION['frageCount']);
