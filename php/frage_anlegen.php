9fd9d4cc6685de63a3381c0680e79327ba37ffe5
yanko13 <92911888+yanko13@users.noreply.github.com>
2021-12-09 11:47:48 +0100

    Update frage_anlegen.php

--- php/frage_anlegen.php
+++ php/frage_anlegen.php
@@ -466,7 +466,7 @@
                             <table>
                                 <tr>
                                     <th></th>
-                                    <th>Frage</th>
+                                    <th class='links'>Frage</th>
                                 </tr>";
                         if ($alleFragenArray != 0)
                         {        
